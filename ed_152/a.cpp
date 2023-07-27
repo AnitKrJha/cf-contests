4b3678025fd3542bd5aa92f177d5250c995e9164
anitkrjha <anitkjha31@gmail.com>
2023-07-28 00:28:00 +0530

    attempted cf_edu_round_152

---
+++ ed_152/a.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fori(i, n) for (int i = 0; i < (n); ++i)
+#define forr(i, a, b) for (int i = (a); i <= (b); ++i)
+#define ford(i, a, b) for (int i = (a); i >= (b); --i)
+template <typename T>
+void printVector(const vector<T> &vec)
+{
+    for (const auto &element : vec)
+    {
+        cout << element << " ";
+    }
+    cout << "\n";
+}
+
+int gcd(int a, int b)
+{
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+vector<int> getPrimeFactors(int n)
+{
+    vector<int> factors;
+    for (int i = 2; i * i <= n; ++i)
+    {
+        while (n % i == 0)
+        {
+            factors.push_back(i);
+            n /= i;
+        }
+    }
+    if (n > 1)
+    {
+        factors.push_back(n);
+    }
+    return factors;
+}
+
+bool isPrime(int n)
+{
+    if (n < 2)
+        return false;
+    for (int i = 2; i * i <= n; ++i)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void solve()
+{
+    int b, c, h;
+    cin >> b >> c >> h;
+    b--;
+    c = c + h;
+    int ans = min(b, c) * 2 + 1;
+    cout << ans << "\n";
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
---
+++ ed_152/b.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fori(i, n) for (int i = 0; i < (n); ++i)
+#define forr(i, a, b) for (int i = (a); i <= (b); ++i)
+#define ford(i, a, b) for (int i = (a); i >= (b); --i)
+template <typename T>
+void printVector(const vector<T> &vec)
+{
+    for (const auto &element : vec)
+    {
+        cout << element << " ";
+    }
+    cout << "\n";
+}
+
+int gcd(int a, int b)
+{
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+vector<int> getPrimeFactors(int n)
+{
+    vector<int> factors;
+    for (int i = 2; i * i <= n; ++i)
+    {
+        while (n % i == 0)
+        {
+            factors.push_back(i);
+            n /= i;
+        }
+    }
+    if (n > 1)
+    {
+        factors.push_back(n);
+    }
+    return factors;
+}
+
+bool isPrime(int n)
+{
+    if (n < 2)
+        return false;
+    for (int i = 2; i * i <= n; ++i)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll k;
+    cin >> k;
+    vector<ll> m(n);
+    vector<ll> indices;
+    for (ll i = 0; i < n; i++)
+    {
+        indices.push_back(i + 1);
+        cin >> m[i];
+        if (m[i] % k == 0)
+            m[i] = k;
+        else
+            m[i] = m[i] % k;
+    }
+
+    sort(indices.begin(), indices.end(), [&](ll x, ll y) -> bool
+         {  if(m[x-1]==m[y-1])
+         return x<y;
+            return m[x - 1] > m[y - 1]; });
+
+    printVector(indices);
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
